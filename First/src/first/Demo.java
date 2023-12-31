b9a93a9ba0579b6cce7de747950e2018ee0c213f
SAKE SREENATH <sakesreenath789@gmail.com>
2023-12-31 23:41:51 +0530

    BASIC PROGRAMS OF JAVA
    All these Programs belongs to Basic Programs of Java

---
+++ First/src/first/Demo.java
@@ -0,0 +1,6 @@
+package first;
+public class Demo {
+	public static void main(String[] args) {
+System.out.println("Hello World!");
+	}
+}
---
+++ HelloWorld/src/com/kodnest/MyFirstJavaProgram/Programming/First.java
@@ -0,0 +1,8 @@
+package com.kodnest.MyFirstJavaProgram.Programming;
+
+public class First {
+	public static void main(String[] args) {
+		System.out.println("Hello,World!");
+	}
+
+}
