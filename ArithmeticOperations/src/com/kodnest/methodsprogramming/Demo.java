4ac1fa93569cd5adcd60e36e28f11ff60474dd53
SAKE SREENATH <sakesreenath789@gmail.com>
2024-01-01 00:26:16 +0530

    PROGRAMS ON METHODS IN JAVA
    This Program belongs to Methods In Java

---
+++ ArithmeticOperations/src/com/kodnest/methodsprogramming/Demo.java
@@ -0,0 +1,21 @@
+package com.kodnest.methodsprogramming;
+
+public class Demo {
+	public static void main(String []args)
+	{
+		long num1=125678L;
+		long num2=9876543210L;
+	
+		long res=galacticAddition(num1,num2);
+		System.out.println(res);
+	}
+	public static long galacticAddition(long num1,long num2)
+	{
+		long a=num1+num2;
+		
+		return a;
+		
+		
+	}
+
+}
---
+++ ArithmeticOperations/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module ArithmeticOperation0308 {
+}
