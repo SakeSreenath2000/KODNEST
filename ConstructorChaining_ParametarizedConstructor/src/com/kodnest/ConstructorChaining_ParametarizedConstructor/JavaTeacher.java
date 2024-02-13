bbf74041e7ee1709cfc5e3957bec4e418d4086b5
SAKE SREENATH <sakesreenath789@gmail.com>
2024-02-13 19:26:26 +0530

    PROGRAM ON CONSTRUCTOR CHAINING USING CONSTRUCTOR
    This program belongs to Constructor Chaining Using Parameterized Constructor

---
+++ ConstructorChaining_ParametarizedConstructor/src/com/kodnest/ConstructorChaining_ParametarizedConstructor/JavaTeacher.java
@@ -0,0 +1,11 @@
+package com.kodnest.ConstructorChaining_ParametarizedConstructor;
+
+public class JavaTeacher extends Teacher{
+	String subject="Java";
+	public JavaTeacher()
+	{
+		super("Kodnest");
+	}
+
+
+}
